b87eca48a734ca0d393104e67e55cec731ad9edc
briangalarza <briangalarza@hotmail.com>
2020-03-11 07:54:19 -0300

    Primer commit

---
+++ app/src/main/java/com/briangalarza/androidarchitectures/ArchitectureActivity.java
@@ -0,0 +1,16 @@
+package com.briangalarza.androidarchitectures;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ArchitectureActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_architectures);
+
+
+    }
+}
